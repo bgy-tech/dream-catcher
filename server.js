f1243e0b5f79dc862fc2e1c78cd4d9f3d1686330
bgy-tech <bogyadavid13@gmail.com>
2026-09-14 14:20:56 +0300

    /health endpoint added

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
+import 'dotenv/config'
 import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import helmet from 'helmet'
 import { initDatabase } from './config/database-init.js';
 import dreamsRouter from './routes/dreams.js';
-
+import pool from './config/database.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const app = express();
 
 // Add securiy headers
@@ -21,6 +21,24 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.static(join(__dirname, 'public')));
 
+//health endpoint
+app.get('/health', async (req, res)=>{
+  try{
+    await pool.query('SELECT 1');
+    res.status(200).json({
+      status:'ok',
+      db:'connected',
+      uptime: process.uptime()
+    });
+  }catch (err){
+    res.status(503).json({
+      status: 'error',
+      db:'disconnected',
+      message: err.message,
+      uptime: process.uptime()
+    })
+  }
+});
 // API Routes
 app.use('/api/dreams', dreamsRouter);
 
